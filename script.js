29b2031279cb7201269712e02158fa8ab516392d
Alexis <alex10o@hotmail.com>
2022-10-27 21:53:59 -0300

    actualizacion

--- script.js
+++ script.js
@@ -7,27 +7,30 @@ console.log(categoria.value);
 console.log(categoria.textContent)
 console.log(categoria.innerHTML)
 
+console.log(cantidad.value);
+console.log(cantidad.textContent)
+console.log(cantidad.innerHTML)
 
-
-function datoInput(){
+function calcularTotal(){
    console.log("probando")
 
-   
+   console.log(cantidad.value);
+   console.log(categoria.value);
 
-   document.getElementById('resultadoInput').textContent = botonUnico.value;
+   // document.getElementById('resultadoInput').textContent = botonUnico.value;
 
-   let valorBoton1 = botonUnico.value;
+   // let valorBoton1 = botonUnico.value;
 
-   if(valorBoton1>15){
-      console.log("es mayor a 15")
-   }
-
-
-   let suma = valorBoton1 + valorBoton1;
+   // if(valorBoton1>15){
+   //    console.log("es mayor a 15")
+   // }
 
 
+   // let suma = valorBoton1 + valorBoton1;
 
-   console.log(suma);
+
+
+   // console.log(suma);
 
 }
 
