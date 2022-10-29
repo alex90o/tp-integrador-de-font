e91729b3a878c6d4682fbbde7f76efa195066f65
Alexis <alex10o@hotmail.com>
2022-10-29 02:38:25 -0300

    se agrego el calculo de las entradas

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 let categoria = document.getElementById('categoria');
 let cantidad = document.getElementById('cantidad');
 let totalapagar = document.getElementById('totalapagar');
+let botonresumen = document.getElementById('botonresumen');
+let resultado = document.getElementById('resultado');
+/*
 console.log(categoria.value);
 console.log(categoria.textContent)
 console.log(categoria.innerHTML)
@@ -11,27 +14,41 @@ console.log(cantidad.value);
 console.log(cantidad.textContent)
 console.log(cantidad.innerHTML)
 
+console.log(botonresumen.value);
+console.log(botonresumen.textContent)
+console.log(botonresumen.innerHTML)
+
+console.log(totalapagar.value);
+console.log(totalapagar.textContent)
+console.log(totalapagar.innerHTML)
+*/
 function calcularTotal(){
-   console.log("probando")
+   //console.log("probando")
 
-   console.log(cantidad.value);
-   console.log(categoria.value);
+   //console.log(cantidad.value);
+   //console.log(categoria.value);
+   let total = 0;
+   //1 Estudiante -- 2 Junior -- 3 Trainee
+   switch (categoria.value) {
+      case "1":
+         total= (200 - (200*0.8))*cantidad.value;
+
+         break;
+      case "2":
+            total= (200 - (200*0.5))*cantidad.value;
    
+         break;   
+         case "3":
+            total=(200 - (200*0.15))*cantidad.value;
+   
+            break;   
+   
+  
+   }
 
-   // document.getElementById('resultadoInput').textContent = botonUnico.value;
-
-   // let valorBoton1 = botonUnico.value;
-
-   // if(valorBoton1>15){
-   //    console.log("es mayor a 15")
-   // }
-
-
-   // let suma = valorBoton1 + valorBoton1;
-
-
-
-   // console.log(suma);
+ //  console.log(total);
+  resultado.innerHTML ="  "+total;
+ 
 
 }
 
